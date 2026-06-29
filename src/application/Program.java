02f8db7c80f79881b56bedaf94ee258ec3d82563
Cristian <cristiancerqueira573@gmail.com>
2026-06-29 09:45:25 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,22 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1,"paulo");
-		System.out.println(obj);
+		Department obj = new Department(1,"Books");
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/YYYY");
+		
+		Seller obj1 = new Seller(1, "João", "joaoPedro@gmail.com", new Date(), 2000.0,obj);
 
+		System.out.println(obj1);
 	}
 
 }
