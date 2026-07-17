3764831a05299f2b7a739ee6e7c8325bd0c79205
Cristian <cristiancerqueira573@gmail.com>
2026-07-17 11:33:32 -0300

    Implement Department insert

---
+++ src/application/program2.java
@@ -0,0 +1,21 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		System.out.println("---------- Test 1: Insert Department");
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		Department department = new Department(null, "Sports");
+		departmentDao.insert(department);
+		
+		
+		
+		
+		
+	}
+
+}
